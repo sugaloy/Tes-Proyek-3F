69a671a89caf6f990e7c52b7516206cdcb50b87f
hkim2403 <ahmadhakim540@gmail.com>
2025-03-19 10:32:00 +0700

    perubahan hakim

--- codes.py
+++ codes.py
@@ -1,7 +1,16 @@
 import os
+#<<<<<<< Updated upstream
 a = 50
 b = 3
 hasil = a + b
 print(hasil)  
 print("Start")
 print("ppppp")
+#=======
+a = 1
+b = 2
+x = a*b
+print (x)
+print("Start")
+print("Start")
+#Stashed changes
