c81527420fb6bd2066026cb007f2a79bd605dd70
RamaSeptian <Ramaseptian045@gmail.com>
2025-04-14 23:11:42 +0700

    Rama Tambahan

--- codes.py
+++ codes.py
@@ -1,28 +1,28 @@
 import os
-#<<<<<<< Updated upstream
+#Bagian 1
 a = 50
 b = 3
 hasil = a + b
-print(hasil)  
+print("Hasil a+b :", hasil)  
 print("Start")
 print("ppppp")
 if(hasil<100) :
-    print("Hasil Kurang dari Seratus")
+    print("Pernyataan :Hasil Kurang dari Seratus")
 if(hasil>100) :
-    print("Hasil Lebih dari Seratus")
-#=======
+    print("Pernyataan :Hasil Lebih dari Seratus")
+#Bagian 2
 a = 1
 b = 2
 x = a*b
-print (x)
+print ("Hasil a*b :",x)
 print("Start")
 print("Start")
 print("Mulai")
-#Stashed changes
+#Bagian 3
 print('tambah baru')
 c = 4
 d = 1
 y = c-d
-print (y)
+print ("Hasil c-d:",y)
 
 
